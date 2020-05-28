14a1011a72e687eede7a6eb269a2f227ef1d3dcb
Neel Mazumdar <neelmazumdar7@gmail.com>
2020-05-27 20:57:12 -0400

    added del

--- data/seeds/002_products.js
+++ data/seeds/002_products.js
@@ -1,6 +1,6 @@
 
 exports.seed = async function(knex) {
-  await knex("products").truncate()
+  await knex("products").del()
 	await knex("products").insert([
 		{id: 1, name: 'banana', description: 'fruit', market_location: 'wakanda', quantity: '3', price: '50', user_id: 1},
     {id: 2, name: 'goat', description: 'livestock', market_location: 'wakanda', quantity: '3', price: '50', user_id: 1},
